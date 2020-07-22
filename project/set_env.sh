37ff884089eda0e22f217da19a341a729a51d8aa
KarthikB96 <balasubramanya.k@husky.neu.edu>
2020-07-21 23:19:00 -0400

    Microservices created

--- project/set_env.sh
+++ project/set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=
-export POSTGRES_PASSWORD=
-export POSTGRES_HOST=
-export POSTGRES_DB=
-export AWS_BUCKET=
-export AWS_REGION=
-export AWS_PROFILE=
-export JWT_SECRET=
+export POSTGRES_USERNAME=karthik
+export POSTGRES_PASSWORD=12345678
+export POSTGRES_HOST=cdr.ctfi4hb3j7tt.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udagram247576660009dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello
 export URL=http://localhost:8100
